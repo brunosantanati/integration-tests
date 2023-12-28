91b57e354bc44deb95b7a77106f84a04d625d95f
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-12-27 22:14:07 -0300

    Empty project for WireMock examples

---
+++ wiremock/src/main/java/me/brunosantana/wiremock/WiremockApplication.java
@@ -0,0 +1,13 @@
+package me.brunosantana.wiremock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiremockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiremockApplication.class, args);
+	}
+
+}
---
+++ wiremock/src/test/java/me/brunosantana/wiremock/WiremockApplicationTests.java
@@ -0,0 +1,13 @@
+package me.brunosantana.wiremock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiremockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
